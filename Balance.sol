6a1cf6fcd79e44a632db2c1a10c931a898bb3814
Sumith prabhu <91826109+sumithprabhu@users.noreply.github.com>
2023-01-24 23:04:46 +0530

    contract updated

--- Balance.sol
+++ Balance.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.1;
 
 contract Payable{
-//payable keyword means that the function can process transactions with non-zero Ether value
+
 address payable public owner;
 constructor () {
 // owner address should be payable
