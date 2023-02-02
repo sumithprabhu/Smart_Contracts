38738f5cad9da553b2d62b0b5c5faeffbe323df0
Sumith prabhu <91826109+sumithprabhu@users.noreply.github.com>
2023-02-02 23:17:46 +0530

    code updated

--- TableOfx.sol
+++ TableOfx.sol
@@ -6,8 +6,7 @@ contract Table{
 
   uint[]  data; 
   
-    // Defining a function 
-    // to demonstrate 'For loop'
+   
     function table(uint x,uint len) public returns(uint[] memory){
     for(uint i=1; i<len; i++){
         uint res = x * i;
